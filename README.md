a4787bb8d29d94f09bbc2b398d0ce779ab88505b
Alexandre Software Developer <94297336+alexandrealecode@users.noreply.github.com>
2022-02-18 08:08:10 -0300

    Ambiente web/mobile reponsivo

---
+++ README.md
@@ -0,0 +1,5 @@
+https://portfolio-dev-peach.vercel.app/
+
+# Portfolio Responsive Complete
+### Portfolio responsive complete
+##HTML ##CSS ##JAVASCRIPT
---
+++ assets/js/main.js
@@ -0,0 +1,76 @@
+/*===== MENU SHOW =====*/ 
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+/*===== ACTIVE AND REMOVE MENU =====*/
+const navLink = document.querySelectorAll('.nav__link');   
+
+function linkAction(){
+  /*Active link*/
+  navLink.forEach(n => n.classList.remove('active'));
+  this.classList.add('active');
+  
+  /*Remove menu mobile*/
+  const navMenu = document.getElementById('nav-menu')
+  navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '800px',
+    duration: 2000,
+    reset: true
+});
+
+/*SCROLL HOME*/
+sr.reveal('.home__title',{}); 
+sr.reveal('.button',{delay: 200}); 
+sr.reveal('.nav__logo',{delay: 400}); 
+sr.reveal('.home__social-icon',{ interval: 200}); 
+
+/*SCROLL ABOUT*/
+sr.reveal('.about__img',{}); 
+sr.reveal('.about__subtitle',{delay: 400}); 
+sr.reveal('.about__text',{delay: 400}); 
+
+/*SCROLL SKILLS*/
+sr.reveal('.skills__subtitle',{}); 
+sr.reveal('.skills__text',{}); 
+sr.reveal('.skills__data',{interval: 200}); 
+sr.reveal('.skills__img',{delay: 600});
+
+/*SCROLL WORK*/
+sr.reveal('main__card__img',{interval: 200}); 
+
+
+
+
+//Funçõe
+function funcao1()
+{
+alert("Deseja mesmo desconectar!");
+}
+function funcao2()
+{
+alert("Você acessou a pasta meus Eventos");
+}
+function funcao3()
+{
+alert("Você acessou a pasta de Usuários!");
+}
+function funcao4()
+{
+alert("Você acessou a pasta de Relatórios!");
+}
+
